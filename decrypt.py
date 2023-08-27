b162d902036aabf8b1c56953678b4ab63e0928a1
Nilesh saini <87272231+Nikki0312@users.noreply.github.com>
2023-08-27 11:10:06 +0530

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,9 +1,10 @@
-def decrypt(decryptMessage):
+def decrypt(decryptMessage,shift):
     for dec in decryptMessage:
         ascci=ord(dec)
-        ascci=ascci-4
+        ascci=ascci-shift
         print(chr(ascci),end="")
 
 
 message=input("Enter your encrpted message: ")
-decrypt(message)
+shift_number=int(input("Enter a shift number to decode: "))
+decrypt(message,shift_number)
